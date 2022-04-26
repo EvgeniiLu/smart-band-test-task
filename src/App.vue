2c04ebf31d85ed4c6cdf9ffae9b713cbccf8080b
Lushchai Evgenii <lushayevgen@mail.ru>
2022-04-27 00:56:50 +0300

    add fullageuser

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@
       <img :src="user.avatar" />
     </div>
     <div class="user-name">{{ user.first_name }}{{ user.last_name }}</div>
-    <div class="user-age">{{ user.date_of_birth }}</div>
+    <div class="user-age">{{ fullAgeUser }}</div>
     <div class="user-employment">{{ user.employment.title }}</div>
   </div>
   <div class="beer" v-if="beer">
-    <div class="beer-brand">Производитель {{ beer.brand }}</div>
-    <div class="beer-name">Название {{ beer.name }}</div>
-    <div class="beer-alcohol">Содержание алкоголя {{ beer.alcohol }}</div>
+    <div class="beer-brand">Производитель: {{ beer.brand }}</div>
+    <div class="beer-name">Название: {{ beer.name }}</div>
+    <div class="beer-alcohol">Содержание алкоголя: {{ beer.alcohol }}</div>
   </div>
 
   <button @click="clickFunc">Клик</button>
@@ -32,19 +32,18 @@ export default {
 
   created: async function () {
     this.user = await loadUser();
-    this.fullAgeUser();
     this.beer = await loadBeer();
   },
-
+  computed: {
+    fullAgeUser() {
+      let dateBirth = new Date(this.user.date_of_birth);
+      let date = new Date();
+      return Math.floor((date - dateBirth) / (60 * 60 * 24 * 365 * 1000));
+    },
+  },
   methods: {
     async clickFunc() {
       this.beer = await loadBeer();
-    },
-
-    fullAgeUser() {
-      let date = new Date();
-      console.log(date);
-      console.log(this.user.date_of_birth);
     },
   },
 };
